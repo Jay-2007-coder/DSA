170f4ac8ff1f5ddff473d1a1472becd41463fa52
Jay-2007-coder <jaytalekar82@gmail.com>
2026-07-18 17:15:27 +0530

    Starting with array

--- Arrays/arr.java
+++ Arrays/arr.java
@@ -1,5 +1,14 @@
-package Arrays;
-
+import java.util.*;
 public class arr {
-    
+    public static void main(String[] args) {
+        int marks[] = new int[5];
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<marks.length; i++){
+            marks[i] = sc.nextInt();
+        }
+       for(int i=0; i<marks.length; i++){
+            System.out.println(marks[i]);
+        } 
+
+     }
 }
