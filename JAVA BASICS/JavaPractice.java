1beddcb8226ee0444c36102c9dc1d7c5d7944652
Jay-2007-coder <jaytalekar82@gmail.com>
2026-07-18 16:49:04 +0530

    DSA

---
+++ JAVA BASICS/JavaPractice.java
@@ -0,0 +1,9 @@
+public class JavaPractice {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = (a+b+c)/3;
+        System.out.println("The average is: " + d);
+    }
+}
