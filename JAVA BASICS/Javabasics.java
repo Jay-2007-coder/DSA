598abfd3fe53f3a2527fd6c063237199ddadcb03
Jay-2007-coder <jaytalekar82@gmail.com>
2026-08-13 05:59:46 +0530

    Add inverted number triangle pattern

--- JAVA BASICS/Javabasics.java
+++ JAVA BASICS/Javabasics.java
@@ -1507,3 +1507,15 @@
 // }
 // }
 // }
+
+// public class Javabasics {
+// public static void main(String args[]) {
+// for (int line = 1; line <= 5; line++) {
+// for (int star = 1; star <= 5 - line + 1; star++) {
+// System.out.print(star);
+
+// }
+// System.out.println();
+// }
+// }
+// }
