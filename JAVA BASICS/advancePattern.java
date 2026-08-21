6abb6781b2ae4ba0eb4dbd35108e165c9fbc5b72
Jay-2007-coder <jaytalekar82@gmail.com>
2026-08-21 16:58:22 +0530

    add butterfly and solid rhombus patterns

--- JAVA BASICS/advancePattern.java
+++ JAVA BASICS/advancePattern.java
@@ -1,9 +1,4 @@
 public class advancePattern {
-    public static void main(String[] args) {
-        Hollow_Rectangle(4, 5);
-
-    }
-
     public static void Hollow_Rectangle(int totalrow, int totalcol) {
         for (int i = 1; i <= totalrow; i++) {
             for (int j = 1; j <= totalcol; j++) {
@@ -17,4 +12,104 @@ public class advancePattern {
             System.out.println();
         }
     }
+
+    public static void inverted_rotated_hal_pyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Inverted_half_pyramid_numbers(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i + 1); j++) {
+                System.out.print(j + "");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void Floyds_Triangle(int n) {
+        int count = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void Triangle01(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void butterfly(int n) {
+
+        // First half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Second Half
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void solidrombus(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (n - i + 1); j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        // inverted_rotated_hal_pyramid(5);
+        // Inverted_half_pyramid_numbers(5);
+        // Floyds_Triangle(5);
+        // Triangle01(5);
+        // butterfly(4);
+        solidrombus(5);
+
+    }
+
 }
