88e99144d1b76805b38da3490c039144572ab11e
Jay-2007-coder <jaytalekar82@gmail.com>
2026-08-21 13:34:48 +0530

    add hollow rectangle pattern

--- JAVA BASICS/advancePattern.java
+++ JAVA BASICS/advancePattern.java
@@ -1,8 +1,18 @@
 public class advancePattern {
     public static void main(String[] args) {
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+        Hollow_Rectangle(4, 5);
+
+    }
+
+    public static void Hollow_Rectangle(int totalrow, int totalcol) {
+        for (int i = 1; i <= totalrow; i++) {
+            for (int j = 1; j <= totalcol; j++) {
+                if (i == 1 || i == totalrow || j == 1 || j == totalcol) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+
             }
             System.out.println();
         }
