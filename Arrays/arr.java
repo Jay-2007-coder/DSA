84686784d7e0591c1d53a9169e16392444892f87
Jay-2007-coder <jaytalekar82@gmail.com>
2026-08-23 01:26:13 +0530

    binary search implementation

--- Arrays/arr.java
+++ Arrays/arr.java
@@ -205,13 +205,13 @@
 
 //Binary Search
 public class arr {
-    public static void binarySearch(int array[], int key) {
+    public static int binarySearch(int array[], int key) {
         int start = 0;
         int end = array.length - 1;
         while (start <= end) {
             int mid = (start + end) / 2;
             if (array[mid] == key) {
-                System.out.println("Element found at index " + mid);
+                return mid;
             }
             if (array[mid] > key) {
                 end = mid - 1;
@@ -219,11 +219,17 @@ public class arr {
                 start = mid + 1;
             }
         }
+        return -1;
     }
 
     public static void main(String args[]) {
         int array[] = { 2, 4, 6, 8, 10, 12, 14 };
         int key = 10;
-        binarySearch(array, key);
+        int index = binarySearch(array, key);
+        if (index == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index " + index);
+        }
     }
 }
