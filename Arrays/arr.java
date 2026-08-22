92b34e617ea8587506833b841c52b0c8bfa9a75f
Jay-2007-coder <jaytalekar82@gmail.com>
2026-08-22 16:04:37 +0530

    arrays basics

--- Arrays/arr.java
+++ Arrays/arr.java
@@ -1,6 +1,88 @@
 
+// public class arr {
+// public static void main(String[] args) {
+// int array[];
+// array = new int[50];
+// System.out.println(array.length);
+// }
+// }
+
+// public class arr {
+// public static void main(String[] args) {
+// int numbers[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+// System.out.println(numbers.length);
+
+// }
+// }
+
+// public class arr {
+// public static void main(String[] args) {
+// String numbers[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+// System.out.println(numbers.length);
+
+// }
+// }
+
+// public class arr {
+// public static void main(String[] args) {
+// int marks[] = new int[100];
+// marks[0] = 98;
+// marks[1] = 76;
+// marks[2] = 85;
+// marks[3] = 95;
+// marks[4] = 89;
+// for (int i = 0; i < marks.length; i++) {
+// System.out.println(marks[i]);
+// }
+
+// }
+// }
+
+// import java.util.*;
+
+// public class arr {
+// public static void main(String[] args) {
+// int marks[] = new int[50];
+// Scanner sc = new Scanner(System.in);
+// marks[0] = sc.nextInt();
+// marks[1] = sc.nextInt();
+// marks[2] = sc.nextInt();
+// marks[3] = sc.nextInt();
+// marks[4] = sc.nextInt();
+// System.out.println("Physics : " + marks[0]);
+// System.out.println("Chemistry : " + marks[1]);
+// System.out.println("Maths : " + marks[2]);
+// System.out.println("English : " + marks[3]);
+// System.out.println("Computer : " + marks[4]);
+// marks[3] = 5; // update
+// System.out.println("English : " + marks[3]);
+// marks[3] = marks[2]; // update
+// System.out.println("English : " + marks[3]);
+
+// }
+// }
+
+// import java.util.*;
+
+// public class arr {
+// public static void main(String[] args) {
+// int marks[] = new int[50];
+// Scanner sc = new Scanner(System.in);
+// marks[0] = sc.nextInt();
+// marks[1] = sc.nextInt();
+// marks[2] = sc.nextInt();
+// System.out.println("Physics : " + marks[0]);
+// System.out.println("Chemistry : " + marks[1]);
+// System.out.println("Maths : " + marks[2]);
+// int percentage = ((marks[0] + marks[1] + marks[2]) / 3);
+// System.out.println("percentage" + percentage + "%");
+// }
+// }
+
 public class arr {
     public static void main(String[] args) {
+        int marks[] = new int[50];
+        System.out.println(marks.length);
 
     }
 }
