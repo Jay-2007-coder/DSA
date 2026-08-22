bb19fb35590ad663e5c302f35925bce3252dcdf9
Jay-2007-coder <jaytalekar82@gmail.com>
2026-08-23 01:17:15 +0530

    binary search

--- Arrays/arr.java
+++ Arrays/arr.java
@@ -183,22 +183,47 @@
 
 // 
 
-import java.util.*;
+// import java.util.*;
 
+// public class arr {
+//     public static int SmallestNumber(int array[]) {
+//         int smallest = Integer.MAX_VALUE; // +infinity
+//         for (int i = 0; i < array.length; i++) {
+//             if (array[i] < smallest) {
+//                 smallest = array[i];
+//             }
+//         }
+//         return smallest;
+
+//     }
+
+//     public static void main(String args[]) {
+//         int array[] = { 1, 2, 6, 3, 5, 0 };
+//         System.out.println(SmallestNumber(array));
+//     }
+// }
+
+//Binary Search
 public class arr {
-    public static int SmallestNumber(int array[]) {
-        int smallest = Integer.MAX_VALUE; // +infinity
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < smallest) {
-                smallest = array[i];
+    public static void binarySearch(int array[], int key) {
+        int start = 0;
+        int end = array.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (array[mid] == key) {
+                System.out.println("Element found at index " + mid);
+            }
+            if (array[mid] > key) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
-        return smallest;
-
     }
 
     public static void main(String args[]) {
-        int array[] = { 1, 2, 6, 3, 5, 0 };
-        System.out.println(SmallestNumber(array));
+        int array[] = { 2, 4, 6, 8, 10, 12, 14 };
+        int key = 10;
+        binarySearch(array, key);
     }
 }
